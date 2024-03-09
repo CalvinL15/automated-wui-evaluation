3963481e481016c7dc9ba710758cbaa2b8a91762
Calvin Liusnando <liusnandoc@gmail.com>
2024-03-10 00:46:12 +0100

    WUI-10032024-00:45: add backend api for retrieving metrics info

--- backend/server.py
+++ backend/server.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85b58c78435361a047cfec9c2024032c206feeef138dda3c3799602deb73ca3b
-size 93
+oid sha256:7857d64ec944599b1530cb82c23c542afd4b7429aeacfd972f1733d16fab49b1
+size 766
