66f0ae9f45d1fe298870786eb2925110a0fba098
Calvin Liusnando <liusnandoc@gmail.com>
2024-03-09 23:16:27 +0100

    WUI-09042024-23:15: finalize metrics.json, updates in python dependencies

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1253fe3e7f1bd4376f82c788526e6c7e8ed5326bcd58b2154cd206da0d13f4b5
-size 829
+oid sha256:514c21022e066aaf0103350f30e04753994eb08e780cd16d2eca39880b1544ab
+size 903
--- backend/src/segmentation_aim/model.py
+++ backend/src/segmentation_aim/model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6276acb3be8d0ff4baf88f0748ba9c98e40518e0970dee76e11d61c0f52f841f
-size 15130
+oid sha256:b478848d095254a35a81f5ed2225688effd6345dfd9fde05819a5d1169f87145
+size 15208
