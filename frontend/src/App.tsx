2585c145641ac9ade428ff2ee14a1660439c3005
Calvin Liusnando <liusnandoc@gmail.com>
2024-03-10 23:50:54 +0100

    WUI-10032024-23:50: add template for MetricExtensionPage.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00f3af74a0854276e2838bcdf18f5948950f112f1f91eeee53f42799b826c51d
-size 1809
+oid sha256:577213ffa0ce2f9d60f75c1433c2639ecfd0469275d20b1894b914fa5a98f4e9
+size 2347
---
+++ frontend/src/components/metrics-extension/MetricExtensionPage.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3d708739275e990a78766d42539119ed8902d37ebd3fe4b45eeedc9b18440f89
+size 2563
