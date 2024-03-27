dc05d4d5358c95baa757d876b81bebd7900d7c3a
Calvin Liusnando <liusnandoc@gmail.com>
2024-03-27 10:07:10 +0100

    WUI-27032024-10:08: update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab6acf7dfa8c1be3a92471d8fe45294cfa57f59f365a3d1a347612867dd96ecd
-size 888
+oid sha256:f2434b7326c53183cf718068376ac252a46e07f695c0ea304c42609b84c49a17
+size 918
