0da5e72f56f58f54d9833955c74f2ee6d28774de
Calvin Liusnando <liusnandoc@gmail.com>
2024-03-30 02:35:30 +0100

    WUI-30032024-02:35: minor update in Metric Extension Interface

--- frontend/src/components/metrics-extension/MetricExtensionGuide.tsx
+++ frontend/src/components/metrics-extension/MetricExtensionGuide.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f45dc1f9e705c4475f1210ff4a1439e17278bf7c701fa81db60d90d7116d358
+oid sha256:5e233340da725459fcda76fea9765e19b17f5f0a4a6fe532acf58ae44050e53e
 size 5438
---
+++ frontend/src/types/MetricExtensionRequestMetadataType.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5ee9174d47c6a17a085cd5e9a5a967e3553446d88e0433cec53c76d4aaf620cf
+size 108
