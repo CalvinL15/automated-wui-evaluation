b8362136de97b53256033b6f782ce36b60f9812e
Calvin Liusnando <liusnandoc@gmail.com>
2024-04-01 03:34:10 +0200

    WUI-01042024-03:33: fix bug in background tasks

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ad0f47539fdf06d333b4a2ce27938a50c190121a9cb7959463e3c6d0e345fb9
-size 11061
+oid sha256:d64f101cde9c314c8b4053ca22f19b688ebbead36ee0520fad751599ef3872d3
+size 11055
