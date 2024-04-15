7eff692493b0c626c171600dc1090db8ffe73c54
Calvin Liusnando <liusnandoc@gmail.com>
2024-04-16 00:35:16 +0200

    WUI-16042024-00:34: adjust server

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:abd7c79d6cfb98a8dcdf2d53dee646b4b649dfb8b26ab48797ff78887d2244c8
-size 11155
+oid sha256:4b828ab449bc1c14bafc149bbb1ebc5a45cf8acc89765a370b547d3b09a6f227
+size 11121
